f64345fd6ba14d292f97902037ad4d9b2ca3c17e
januwA <ajanuw1995@gmail.com>
2025-10-08 10:33:13 +0800

    升级构建工具版本

--- dist/esm/index.js
+++ dist/esm/index.js
@@ -1,16 +1,8 @@
 import { add as f } from "./add.js";
-import { sub as n } from "./sub.js";
-import { foo as x } from "./foo.js";
-import { CountComponent as a, DynamicJump as d, useCountStore as s } from "./react-shared.js";
-import "react/jsx-runtime";
-import "react";
-import "zustand";
-import "react-router-dom";
+import { sub as m } from "./sub.js";
+import { foo as t } from "./foo.js";
 export {
-  a as CountComponent,
-  d as DynamicJump,
   f as add,
-  x as foo,
-  n as sub,
-  s as useCountStore
+  t as foo,
+  m as sub
 };
--- dist/umd/index.d.ts
+++ dist/umd/index.d.ts
@@ -1,4 +1,3 @@
 export { add } from './add';
 export { sub } from './sub';
 export { foo } from './foo';
-export { useCountStore, CountComponent, DynamicJump } from './react-shared';
